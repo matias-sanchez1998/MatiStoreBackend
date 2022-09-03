85cfb19eefd5390f11c29317f41bb72598af4eba
magomatiassss <magomatiassss@gmail.com>
2022-09-03 16:56:55 -0300

    make it better

---
+++ routes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const routes = express.Router();
+
+routes.get('/', (req, res) => {
+    req.getConnection((err, conn) => {
+        if (err) return res.send(err);
+        conn.query('SELECT * from product', (err, rows) => {
+            if (err) return res.send(err);
+
+            res.json(rows)
+
+        })
+    })
+});
+
+
+
+module.exports = routes
---
+++ server.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const mysql = require('mysql');
+const myconn = require('express-myconnection');
+const cors = require('cors');
+
+
+
+
+const routes = require('./routes');
+
+const app = express();
+app.set('port', process.env.PORT || 9000)
+
+const dbOption = {
+    host: 'mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com',
+    user: 'bsale_test',
+    password: 'bsale_test',
+    database: 'bsale_test',
+
+}
+
+
+var connection = mysql.createConnection(dbOption);
+
+
+function handleDisconnect() {
+
+    connection = mysql.createConnection(dbOption); // Recreate the connection, since
+    connection.connect(() => {
+        console.log('HOLA');
+        connection.query('SET session wait_timeout=3000')                           
+        setTimeout(handleDisconnect, 2000); 
+    });
+  
+    connection.on('error', function (err) {
+        console.log('db error', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            handleDisconnect();                       
+        } else {                                      
+            throw err;                                
+        }
+    });
+}
+
+//middlewares-------------------
+app.use(myconn(mysql, dbOption, 'single'))
+
+app.use(cors());
+
+
+//routes---------------
+app.get('/', (req, res) => {
+    res.send('Welcome to my API')
+})
+
+app.use('/api/product',routes);
+
+app.get('/api/category', (req, res) => {
+    req.getConnection((err, conn) => {
+        if (err) return res.send(err);
+        conn.query('SELECT * from category', (err, rows) => {
+            if (err) return res.send(err);
+
+            res.json(rows)
+            
+        })
+    })
+});
+
+
+
+//server-running---------
+app.listen(app.get('port'), () => {
+    console.log('server running on port ', app.get('port'));
+
+})
+
+
+
+
+handleDisconnect();
